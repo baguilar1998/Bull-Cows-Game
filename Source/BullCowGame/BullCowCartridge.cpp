ff57a24f6eedd2c16e00b257a3b78ff682bec43f
Brian Kenji Aguilar <anjacular@gmail.com>
2020-05-17 11:53:22 -0400

    Working on game logic

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,32 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cows game!"));
+    PrintLine(TEXT("Guess the 5 letter word!")); // Number might change for other words! Cannot be hard coded
+    PrintLine(TEXT("Press enter to start..."));
+    HiddenWord = TEXT("kenji"); 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    // Checks whether the word the user inputted was the correct guess
+    bool GuessIsCorrect = HiddenWord == Input;
+    if (GuessIsCorrect) { 
+        PrintLine(TEXT("You have guessed the right word!"));
+    } else {
+        PrintLine(TEXT("You have guessed the wrong word :("));
+    }
 
+    // Check if isogram
+    // check right number of characters
+
+    // remove life
+
+    // check if lives > 0
+    // if yes play again
+    // if no show game over
+    // prompt to play again
+    // Check user input
+    // play again or quit
 }
